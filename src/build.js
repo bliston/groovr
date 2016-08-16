85f80032472c06e687a4750783084dad28741f83
Brandon Liston <brandon.g.liston@gmail.com>
2016-08-16 17:27:45 -0400

    resolved deprecation warning by using renderToString from react-dom/server

--- src/build.js
+++ src/build.js
@@ -4,13 +4,14 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
 var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 require('node-jsx').install();
 
 var template = _.template(fs.readFileSync(path.join(__dirname, './layouts/default.html'), 'utf8'));
 var App = React.createFactory(require('./components/app.jsx'));
 
 var data = require('./data');
-data.app = React.renderToString(App(data));
+data.app = ReactDOMServer.renderToString(App(data));
 var html = template(data);
 fs.writeFileSync(path.join(__dirname, '../index.html'), html);
 
